b8a04d7bb9b0db1b3579c7038f8b0671c4ac6ebd
Maluki Muthusi <malukimuthusi@gmail.com>
2021-04-05 16:26:29 +0300

    Feat: Server and client communicating over sockets
    They use TCP socket.
    client connects to the server.
    the server writes a message to the clients socket
    client receives the message and outputs to the user screen.

---
+++ part_1/client.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+int main(int argc, char const *argv[])
+{
+
+    // definitions
+    size_t socket_fd;
+    struct sockaddr_in server_address;
+
+    //create a socket
+    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        // TODO: check the errno and return more meaningful error codes
+        printf("socket error: failed to init a socket");
+        exit(1);
+    }
+
+    //specify an address for the socket
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+
+    // Ip address of the server must be provided.
+    if (argc != 2)
+    {
+        printf("Ip address of ther server must be provided\n");
+        exit(1);
+    }
+
+    // convert the IP address from a string(dot notation), to a binary representation
+    if (inet_pton(AF_INET, argv[1], &server_address.sin_addr) <= 0)
+    {
+        if (errno == EAFNOSUPPORT)
+            printf("invalid address provided\n");
+        else
+            printf("error occurred converting address\n");
+
+        exit(1);
+    }
+
+    // establish a tcp connection to the server.
+    if ((connect(socket_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0))
+    {
+        printf("failed to connect socket\n error code: %s\n", strerror(errno));
+        // TODO: check the error codes in errno and return meaningful error message
+        exit(1);
+    }
+
+    //receive data from the server
+    char server_response[256];
+    recv(socket_fd, &server_response, sizeof(server_response), 0);
+
+    //print out the server's response
+    printf("The server sent the data %s\n", server_response);
+
+    //close the socket
+    close(socket_fd);
+
+    return 0;
+}
---
+++ part_1/server.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+int main()
+{
+
+    // definations
+    char server_message[256] = "you have reached the server!";
+    int server_socket_fd;
+    struct sockaddr_in server_address;
+    int client_socket_fd;
+
+    // create the socket
+    if ((server_socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        // TODO: check the errno and return more meaningful error codes
+        printf("socket error: failed to init a socket");
+        exit(1);
+    }
+
+    //specify an address for the socket
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);              // listening port number
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY); // 0.0.0.0
+
+    // TODO(3) use a specic IP address
+
+    //bind the socket to our specified IP and port
+    if ((bind(server_socket_fd, (struct sockaddr *)&server_address, sizeof(server_address))) < 0)
+    {
+        // TODO: check the errno and return more meaningful error codes
+        printf("failed to assign a name to the socket\n");
+        printf("error code: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    // wait for connection
+    if ((listen(server_socket_fd, 5)) < 0)
+    {
+        // TODO: check the errno and return more meaningful error codes
+        printf("failed to mark the fd to be used by the socket");
+        exit(1);
+    }
+
+    if ((client_socket_fd = accept(server_socket_fd, NULL, NULL)) < 0)
+    {
+        // TODO: check the errno and return more meaningful error codes
+        printf("failed to accept connection");
+        exit(1);
+    }
+
+    //send the message
+    send(client_socket_fd, server_message, sizeof(server_message), 0);
+
+    //close socket
+    if (close(server_socket_fd) < 0)
+    {
+        // TODO: check the errno and return more meaningful error codes
+        printf("failed to close client socket");
+        exit(1);
+    }
+
+    // TODO(1): show message that the server is running and waiting for a client connection
+    // TODO(2): fix bug in the client, release all the resources.
+
+    return 0;
+}
