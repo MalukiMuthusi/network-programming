b6cc8d6786ef550987a993f835f3aa1b36c16d1d
Maluki Muthusi <malukimuthusi@gmail.com>
2021-04-08 14:01:27 +0300

    Feat: Create Account feature is good now

--- part_2/client.c
+++ part_2/client.c
@@ -17,6 +17,7 @@ void show_balance(size_t socket_fd);
 void open_account(size_t socket_fd);
 void close_account(size_t socket_fd);
 void withdraw(size_t socket_fd);
+void open_account(const size_t socket_fd);
 
 /* Online Banking application client */
 int main(int argc, char const *argv[])
@@ -102,8 +103,7 @@ int main(int argc, char const *argv[])
     printf("\t1. Balance.\n");
     printf("\t2. Close Account\n");
     printf("\t3. Withdraw.\n");
-    printf("\t4. Open Account\n");
-    printf("\t5. Statements\n");
+    printf("\t4. Statements\n");
 
     // read the users service choice
     char command[8];
@@ -131,8 +131,6 @@ int main(int argc, char const *argv[])
         show_balance(socket_fd);
         break;
     case 2:
-        // open account
-        // open_account(socket_fd);
         close_account(socket_fd);
         break;
     case 3:
@@ -140,10 +138,7 @@ int main(int argc, char const *argv[])
         // close account
         break;
     case 4:
-        // withdraw amount
-        break;
-    case 5:
-        // account statements
+        open_account(socket_fd);
         break;
     default:
         break;
@@ -189,52 +184,96 @@ void show_balance(size_t socket_fd)
 }
 
 /* open a bank account */
-void open_account(size_t socket_fd)
+void open_account(const size_t socket_fd)
 {
     /* write the open account command to the server. */
-
     char buff[8];
-    if (snprintf(buff, sizeof(buff), "%d", 1) < 0)
+    if (snprintf(buff, sizeof(buff), "%d", 4) < 0)
     {
         // TODO: check the errno and return more meaningful error codes
-        printf("failed to write time to the buffer");
+        printf("failed to write open account command to the buffer\n");
         exit(1);
     }
-    if (write(socket_fd, buff, strlen(buff)) < 0)
+    if (send(socket_fd, buff, 8, 0) < 0)
     {
         // TODO: check the errno and return more meaningful error codes
-        printf("failed to write time to the socket");
+        printf("failed to write open account command to the socket\n");
         exit(1);
     }
 
     /* server responds with a message that user can continue */
 
-    int n;
-    char receiveline[MAXLINE + 1];
-    while (((n = read(socket_fd, receiveline, MAXLINE)) > 0))
-    {
-        receiveline[n] = 0; // null terminate
+    /* ask user for their details to open account with */
+    printf("Enter details to create account. \n");
 
-        /* write the balance amount to user output */
-        if (fputs(receiveline, stdout) == EOF)
-        {
-            // TODO: return a better error message
-            printf("error when writing the server's response to the output device\n");
-            exit(1);
-        }
+    char full_name[64];
+    printf("\t Enter first name: ");
+    scanf("%s", full_name);
+
+    char date_birth[64];
+    printf("\tDate of birth:  ");
+    scanf("%s", date_birth);
+
+    printf("Select account type: \n\t 1. Basic Account. \n\t 2. Savings Account.\n");
+    char account_type[8];
+    scanf("%s", account_type);
+
+    /* send user details to the server */
+
+    // send full names
+    if (send(socket_fd, full_name, 64, 0) < 0)
+    {
+        // TODO: check the errno and return more meaningful error codes
+        printf("failed to write full names to the socket\n");
+        exit(1);
     }
-    if (n == -1)
+
+    // send date of birth
+    if (send(socket_fd, date_birth, 64, 0) < 0)
+    {
+        // TODO: check the errno and return more meaningful error codes
+        printf("failed to write date of birth to the socket\n");
+        exit(1);
+    }
+
+    // send account type
+    if (send(socket_fd, account_type, 8, 0) < 0)
+    {
+        // TODO: check the errno and return more meaningful error codes
+        printf("failed to write account type to the socket\n");
+        exit(1);
+    }
+
+    /* server response with a success or failure in opening account */
+    char serv_response_buff[8];
+    if ((recv(socket_fd, serv_response_buff, 8, 0)) == -1)
     {
         // TODO: check errno and return a better error message
         printf("error: reading from the socket\n");
         exit(1);
     }
 
-    /* ask user for their details to open account with */
+    errno = 0;
+    char *endptr;
+    long serv_response = strtol(serv_response_buff, &endptr, 10);
+    if (errno != 0)
+    {
+        perror("Invalid confirmation number Entered\n");
+        exit(EXIT_FAILURE);
+    }
+    if (endptr == serv_response_buff)
+    {
+        fprintf(stderr, "No confirmation number was entered\n");
+        exit(EXIT_FAILURE);
+    }
 
-    /* send user details to the server */
+    if (serv_response != 1)
+    {
+        printf("failed to create account.\n");
+        return;
+    }
 
-    /* server response with a success or failure in opening account */
+    printf("account created\n");
 
     return;
 }
@@ -284,10 +323,8 @@ void close_account(size_t socket_fd)
 
     /* server responds with a message that user can continue */
 
-    int n;
     char serv_response[8];
-    n = recv(socket_fd, serv_response, 8, 0);
-    if (n == -1)
+    if ((recv(socket_fd, serv_response, 8, 0)) == -1)
     {
         // TODO: check errno and return a better error message
         printf("error: reading from the socket\n");
