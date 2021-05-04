2f7e685b4168e8e0cba65c77bc7651353bf5cd87
Maluki Muthusi <malukimuthusi@gmail.com>
2021-05-04 15:37:24 +0300

    Add function to read and write to file
    Use the standard functions, read and write to read and write to sockots.

--- part_2/library/function_headers.h
+++ part_2/library/function_headers.h
@@ -8,6 +8,9 @@ void create_socket(size_t *socket_fd);
 void send_pin(char *pin, socklen_t socket_fd);
 void show_commands();
 long read_command_input();
-void connect_tcp(const size_t socket_fd, void *server_address);
+void connect_tcp(size_t *socket_fd, struct sockaddr_in *server_address);
 void provide_service(long action);
+char *read_pin();
+ssize_t writen(int fd, const void *buffer, size_t n);
+ssize_t readn(int fd, void *buffer, size_t n);
 #endif
--- part_2/library/utils.c
+++ part_2/library/utils.c
@@ -37,10 +37,87 @@ long read_command_input()
 }
 
 // establish a tcp connection
-void connect_tcp(const size_t socket_fd, void *server_address)
+void connect_tcp(size_t *socket_fd, struct sockaddr_in *server_address)
 {
-    if ((connect(socket_fd, (struct sockaddr *)server_address, sizeof(*server_address)) < 0))
+    if ((connect(*socket_fd, (struct sockaddr *)server_address, sizeof(*server_address)) < 0))
     {
         throw_error_exit("failed to connect socket");
     }
+    printf("Successfuly connected to the server!!");
+}
+
+// read pin from std input
+char *read_pin()
+{
+    char *pin;
+    scanf("%s", pin);
+
+    errno = 0;
+    char *endptr;
+    long pin_number = strtol(pin, &endptr, 10);
+    // TODO: validate pin
+    if (errno != 0)
+    {
+        throw_error_exit("Invalid Pin Entered");
+    }
+    if (endptr == pin)
+    {
+        throw_error_exit("No PIN was entered");
+    }
+
+    return pin;
+}
+
+// read from a file descriptor to a buffer
+ssize_t readn(int fd, void *buffer, size_t n)
+{
+    size_t nleft;
+    ssize_t nread;
+    char *ptr;
+
+    ptr = buffer;
+
+    nleft = n; // initial read = 0 and unread/left = n
+    while (nleft > 0)
+    {
+        if ((nread = read(fd, ptr, nleft)) > 0)
+        {
+            if (errno == EINTR)
+                nread = 0;
+            else
+                return -1;
+        }
+        else if (nread == 0)
+            break;
+        nleft -= nread;
+        ptr += nread;
+    }
+
+    return (n - nleft);
+}
+
+// write to a file descriptor
+ssize_t writen(int fd, const void *buffer, size_t n)
+{
+    size_t nleft;
+    ssize_t nwritten;
+    const char *ptr;
+
+    ptr = buffer;
+    nleft = n;
+    while (nleft > 0)
+    {
+        if ((nwritten = write(fd, ptr, n)) <= 0)
+        {
+            if (nwritten < 0 && errno == EINTR)
+            {
+                nwritten = 0;
+            }
+            else
+                return -1;
+        }
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+    return n;
 }
